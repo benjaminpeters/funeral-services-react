61f2c0b2e722d15516dc752f7be1bbfc2190ca7d
Ben Peters <benpeters49@gmail.com>
2018-06-28 18:08:50 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './components/js/Header';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <p className="App-intro">
+          <code>Under Construction</code>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/js/Header.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import '../css/Header.css';
+import logo from '../../assets/images/funeral-services-logo.png';
+
+class Header extends Component {
+  render() {
+    return (
+      <header id="header">
+            <div className="header-inner container">
+              <div className="nav-holder">
+                <div id="top-logo" className="logo order1"><img
+                 src={logo}
+                   alt="funeral-services-logo"/></div>
+                <div className="col order1">
+                </div>
+                <nav id="nav" className="text-uppercase order2">
+                  <ul className="main-navigation">
+                    <li>
+                      <a href="">Home</a>
+                    </li>
+                    <li>
+                      <a className="nav-services" href="#">
+                        Browse Our Services
+                      </a>
+                    </li>
+                    <li>
+                      <a className="nav-write-a-review" href="#">
+                        Write a Review
+                      </a>
+                    </li>
+                    <li>
+                      <a className="nav-read-reviews" href="#">
+                        Read Reviews
+                      </a>
+                    </li>
+                    <li>
+                      <a className="nav-contact" href="#">
+                        Contact
+                      </a>
+                    </li>
+                  </ul>
+                  <ul className="support-navigation">
+                    <li><a className="login" href="#">LOG IN</a></li>
+                    <li><a className="signup" href="#">SIGN UP</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </header>
+    );
+  }
+}
+
+export default Header;
