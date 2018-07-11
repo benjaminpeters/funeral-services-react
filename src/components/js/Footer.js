62d986652b439f437408d3ef438e88bd41b7be0c
Benjamin <benpeters49@gmail.com>
2018-07-10 21:49:19 -0400

    Some minor formatting on make-arrange. And change class to className in some of the html

--- src/components/js/Footer.js
+++ src/components/js/Footer.js
@@ -4,8 +4,8 @@ import '../css/Footer.css';
 class Footer extends Component{
   render(){
     return (
-      <footer id="footer" class="container">
-        <p>&copy; <a href="#">Funeral Services</a>. All Rights Reserved.<span class="sep">|</span><a href="/">Privacy Policy</a></p>
+      <footer id="footer" className="container">
+        <p>&copy; <a href="#">Funeral Services</a>. All Rights Reserved.<span className="sep">|</span><a href="/">Privacy Policy</a></p>
       </footer>
     )
   }
--- src/components/js/Make-Arrangements.js
+++ src/components/js/Make-Arrangements.js
@@ -5,22 +5,19 @@ import '../css/Make-Arrangements.css';
 class MakeArrganements extends Component {
   render() {
     return (
-      <div class="intro-block col col6">
-        <article class="broucher-block">
-          <div class="block-holder">
-            <div class="description-wrap">
+      <div className="intro-block col col6">
+        <article className="broucher-block">
+          <div className="block-holder">
+            <div className="description-wrap">
               <h2>Funeral and Cremation Arrangements.</h2>
-              <div class="hidden-xs">
-                <p>Please select </p>
-              </div>
             </div>
           </div>
-          <ul class="btn-holder">
+          <ul className="btn-holder">
               <li>
-                <a href="#" class="btn btn-default">IMMEDIATE HELP</a>
+                <a href="#" className="btn btn-default">IMMEDIATE HELP</a>
               </li>
               <li>
-                <a href="#" class="btn btn-default">PLAN AHEAD</a>
+                <a href="#" className="btn btn-default">PLAN AHEAD</a>
               </li>
             </ul>
         </article>
