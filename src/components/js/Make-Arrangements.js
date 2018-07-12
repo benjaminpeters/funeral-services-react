d93c428d6962a2087c881ad36d9db46047967770
Benjamin <benpeters49@gmail.com>
2018-07-11 21:38:31 -0400

    Added onclick to expand and shrink what will be the make arrangment form

--- src/components/js/Make-Arrangements.js
+++ src/components/js/Make-Arrangements.js
@@ -4,13 +4,28 @@ import '../css/Make-Arrangements.css';
 
 class MakeArrganements extends Component {
 
-  onItemClick (event) {
-      console.log("click");
+  constructor(props) {
+      super(props);
+      this.state = {
+        clicked: false,
+        arrangeClose: '600px',
+        arrangeOpen: '1200px'
+      };
+
+      this.handleClick = this.handleClick.bind(this);
+    }
+
+  handleClick (event) {
+
+    this.setState({
+      clicked: !this.state.clicked,
+    });
   }
 
   render() {
     return (
-      <div className="intro-block col col6">
+      <div className='intro-block col col6'
+       style={this.state.clicked ? { width: `${this.state.arrangeOpen}` } : { width: `${this.state.arrangeClose}` }}>
         <article className="broucher-block">
           <div className="block-holder">
             <div className="description-wrap">
@@ -18,10 +33,10 @@ class MakeArrganements extends Component {
             </div>
           </div>
           <ul className="btn-holder">
-              <li className="btn btn-default" onClick={this.onItemClick}>
+              <li className="btn btn-default" onClick={this.handleClick}>
                 IMMEDIATE HELP
               </li>
-              <li className="btn btn-default" onClick={this.onItemClick}>
+              <li className="btn btn-default" onClick={this.handleClick}>
                 PLAN AHEAD
               </li>
             </ul>
